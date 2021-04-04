c76af6333cc34976da53a051e4eb096950c791d4
verbal-noun <kaifahsan420@gmail.com>
2021-04-04 12:24:16 +1000

    Adding CRUD operations

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -31,4 +31,38 @@ async function createCourse() {
   console.log(result);
 }
 
-createCourse();
+// Function to query documents
+async function getCourse() {
+  const courses = await Course
+    // .find({ price: { $gte: 10, $lte: 20}})
+    // .find({price: {$in: [10, 20, 30]}})
+    // .find({
+    //   author: "Kaif",
+    //   isPublished: true,
+    // })
+    .find()
+    .or([{ name: "Mosh" }, { isPublished: true }])
+    .limit(10)
+    .sort({ name: 1 })
+    .select({ name: 1, tags: 1 });
+
+  console.log(courses);
+}
+
+async function updateCourse(id) {
+  const course = await Course.findById(id);
+  if (!course) return;
+
+  course.isPublished = true;
+  course.author = "Ahsan";
+
+  //   course.set({
+  //     isPublished: true,
+  //     author: "Ahsan",
+  //   });
+
+  const result = await course.save();
+  console.log(result);
+}
+
+updateCourse("6069157d6ca43264fb5f8635");
